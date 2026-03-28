4804555ecb999e8bdb0c5902a20192aaeca4ce6f
CMAZO <carlos.mazo@proteccion.com.co>
2026-03-28 10:47:26 -0500

    feat: add structure and initial files

---
+++ micro-services/rentals/app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from app.config import Config
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    with app.app_context():
+        from app.models.rental import Rental
+        db.create_all()  # Crea la tabla rentals si no existe
+
+    from app.routes.rental_routes import rental_bp
+    app.register_blueprint(rental_bp)
+
+    return app
