af9f406ce88fe0b71bc5d260c0ba0562c4fe7719
Jhonnathan Ocampo <jsocampod@eafit.edu.co>
2026-04-02 18:39:39 -0500

    refactor: simplify BikeMarker component by removing redundant button and adjusting layout

--- frontend/src/features/Map/components/BikeMarker.tsx
+++ frontend/src/features/Map/components/BikeMarker.tsx
@@ -8,12 +8,7 @@ export default function BikeMarker({ bike }: BikeMarkerProps) {
         <div>
           <p className="font-semibold">Bike</p>
           <p className="font-semibold text-small">ID: {bike.bikeId}</p>
-          <div className="flex gap-2 justify-between items-center">
-            <p className="text-sm text-green-600">Available</p>
-            <button className="px-3 py-1 bg-brand-500 text-white rounded hover:bg-brand-600 max-h-8">
-              Rent
-            </button>
-          </div>
+          <p className="text-sm text-green-600 mt-1">Available</p>
         </div>
       </Popup>
     </Marker>
